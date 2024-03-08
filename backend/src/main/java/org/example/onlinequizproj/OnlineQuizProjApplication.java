f4b6fb7ce282694afbfa599f3a9899b167232d49
danishppatel <danishpatel5113@gmail.com>
2024-03-08 10:10:37 +0530

    Backend folder Structured Created

---
+++ backend/src/main/java/org/example/onlinequizproj/OnlineQuizProjApplication.java
@@ -0,0 +1,16 @@
+package org.example.onlinequizproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class OnlineQuizProjApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(OnlineQuizProjApplication.class, args);
+        System.out.println("hello danish");
+    }
+
+}
---
+++ backend/src/test/java/org/example/onlinequizproj/OnlineQuizProjApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.onlinequizproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineQuizProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
